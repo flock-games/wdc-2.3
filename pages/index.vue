1454442c2c5de77ef09654bd86d95174f4787db6
Tyler Dawson <tddawson@gmail.com>
2025-05-17 11:10:34 -0700

    Uses mux-video component

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
 import "media-chrome";
+import "@mux/mux-video";
 </script>
 
 <template>
   <media-controller>
-    <video
+    <mux-video
+      playback-id="A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg"
       slot="media"
-      src="https://stream.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/high.mp4"
-      poster="https://image.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/thumbnail.jpg"
-    ></video>
+      crossorigin
+      muted
+    ></mux-video>
+    <media-loading-indicator
+      slot="centered-chrome"
+      noautohide
+    ></media-loading-indicator>
     <media-control-bar>
       <media-play-button></media-play-button>
+      <media-seek-backward-button></media-seek-backward-button>
+      <media-seek-forward-button></media-seek-forward-button>
       <media-mute-button></media-mute-button>
+      <media-volume-range></media-volume-range>
       <media-time-range></media-time-range>
-      <media-time-display></media-time-display>
+      <media-time-display showduration remaining></media-time-display>
+      <media-playback-rate-button></media-playback-rate-button>
       <media-fullscreen-button></media-fullscreen-button>
     </media-control-bar>
   </media-controller>
